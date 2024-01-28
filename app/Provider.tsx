80ac41bf1af11910bb8fb102ac3172468cb94d30
eric <ericibrahim19@gmail.com>
2024-01-28 18:34:54 -0300

    commit

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,10 +1,10 @@
 "use client"
-import { SessionProvider } from 'next-auth/react'
 import React from 'react'
+import { SessionProvider } from "next-auth/react"
 
-function Provider({children}) {
+function Provider({ children }: { children: React.ReactNode }) {
   return (
-    <SessionProvider>
+    <SessionProvider >
         {children}
     </SessionProvider>
   )
--- app/Shared/firebaseConfig.js
+++ app/Shared/firebaseConfig.js
@@ -1,19 +1,23 @@
-// Importa solo la función necesaria desde Firebase
-import { initializeApp } from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Configuración de Firebase
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBop18hk3Z5dguQ4c7_o61QYK7_scZrajU",
-  authDomain: "galeryapp-89f8b.firebaseapp.com",
-  projectId: "galeryapp-89f8b",
-  storageBucket: "galeryapp-89f8b.appspot.com",
-  messagingSenderId: "298614001159",
-  appId: "1:298614001159:web:0f3614c3185a360ae764a2",
-  measurementId: "G-PBQ7GY15X3"
+  apiKey: "AIzaSyDsVcT58u5fnibQqpN2sX_OEggbFp0bYqs",
+  authDomain: "tubeguruji-app.firebaseapp.com",
+  projectId: "tubeguruji-app",
+  storageBucket: "tubeguruji-app.appspot.com",
+  messagingSenderId: "887202519021",
+  appId: "1:887202519021:web:584d0b453269ff8df979e4",
+  measurementId: "G-GXD1PVGG7Z"
 };
 
-// Inicializa la aplicación de Firebase
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+ const app = initializeApp(firebaseConfig);
 
+ 
 export default app;
-
