b39a10076c7fc49e31cb6c34c50f616853b66fc0
eric <ericibrahim19@gmail.com>
2024-01-28 19:53:22 -0300

    traer datos de firebase

--- app/[userId]/page.jsx
+++ app/[userId]/page.jsx
@@ -1,10 +1,31 @@
 "use client"
+import { doc, getDoc, getFirestore } from 'firebase/firestore';
 import React, { useEffect } from 'react'
+import app from '../Shared/firebaseConfig';
 
 const Profile = ({params}) => {
+    const db = getFirestore(app);
     useEffect(() =>{
         console.log(params.userId.replace('%40','@'))
+        if(params)
+        {
+            getUserInfo(params.userId.replace('%40','@'))
+        }
     },[params]);
+
+
+    const getUserInfo = async(email) => {
+        const docRef = doc(db, "user", email );
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            console.log("Document data:", docSnap.data());
+        }
+        else {
+            //docSnap.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }
   return (
     <div>Profile</div>
   )
