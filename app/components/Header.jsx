b2b344a86e654a4ec67d2d3a0970cfec9828b873
eric <ericibrahim19@gmail.com>
2024-02-01 16:04:29 -0300

    commit

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -51,8 +51,8 @@ function Header() {
       
 
       {session?.user?  
-      <Image src={session.user.image} 
-      onClick={() => router.push('/'+session.user.email)}
+      <Imagesrc={session.user.image} 
+      onClick={() =router.push('/'+session.user.email)}
       alt='user-image' width={60} height={60}
         className='hover:bg-gray-300 p-2
         rounded-full cursor-pointer'/>:
